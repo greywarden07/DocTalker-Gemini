8633c47f46f57d2ab93ac60e24a745ab5766d0d0
greywarden07 <sadhanagautam200@gmail.com>
2024-03-04 02:05:47 +0530

    Changed the page name

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("Chat PDF")
+    st.set_page_config("DocTalker")
     st.header("DocTalker Gemini💁")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
